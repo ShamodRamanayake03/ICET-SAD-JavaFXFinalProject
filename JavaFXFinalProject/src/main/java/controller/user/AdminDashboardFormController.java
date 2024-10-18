63ca74ec13f811297bd3679d54ace766d57531de
ShamodRamanayake03 <ShamodRamanayake03@gmail.com>
2024-10-18 12:27:36 +0530

    first commit

---
+++ JavaFXFinalProject/src/main/java/controller/user/AdminDashboardFormController.java
@@ -0,0 +1,43 @@
+package controller.user;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AdminDashboardFormController {
+
+    @FXML
+    void btnEmployeeManagementFormOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderManagementFormOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProductManagementFormOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportManagementFormOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManagementFormOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserManagementFormOnAction(ActionEvent event) {
+
+    }
+
+}
